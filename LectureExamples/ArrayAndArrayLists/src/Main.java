645c7da3b794c2260abf727ebb43793d3dc32434
Oguzhan Akbal <44518703+akbaloguzhan@users.noreply.github.com>
2023-07-12 17:58:12 +0300

    Add files via upload

---
+++ LectureExamples/ArrayAndArrayLists/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0, "one");
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        List<String> newList = Arrays.asList("Sunday", "Monday", "Tuesday");
+        System.out.println(newList);
+
+        ArrayList<String> stringLists = new ArrayList<>(List.of("Jan", "Feb", "Mar"));
+        String[] stringArray = stringLists.toArray(new String[0]);
+    }
+}
---
+++ LectureExamples/ArraysProject/src/dev/lpa/Main.java
@@ -0,0 +1,12 @@
+package dev.lpa;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+        System.out.println(myDoubleArray[2]);
+    }
+}
